b64b5b09196eefc87323590a8729c4b0484639af
Gaz79-dev <gary.aitchison@outlook.com>
2025-07-01 09:23:19 +0100

    Update squads.py

--- bot/api/routers/squads.py
+++ bot/api/routers/squads.py
@@ -8,15 +8,10 @@ from pydantic import BaseModel
 
 class RoleUpdateRequest(BaseModel):
     new_role_name: str
-
 class SquadMoveRequest(BaseModel):
     new_squad_id: int
 
-router = APIRouter(
-    prefix="/api/squads",
-    tags=["squads"],
-    dependencies=[Depends(auth.get_current_active_user)],
-)
+router = APIRouter(prefix="/api/squads", tags=["squads"], dependencies=[Depends(auth.get_current_active_user)])
 
 @router.get("/roles", response_model=Dict)
 async def get_all_roles(db: Database = Depends(get_db)):
@@ -25,9 +20,7 @@ async def get_all_roles(db: Database = Depends(get_db)):
 @router.put("/members/{squad_member_id}/role", status_code=204)
 async def update_member_role(squad_member_id: int, request: RoleUpdateRequest, db: Database = Depends(get_db)):
     await db.update_squad_member_role(squad_member_id, request.new_role_name)
-    return
 
 @router.put("/members/{squad_member_id}/move", status_code=204)
 async def move_member_to_squad(squad_member_id: int, request: SquadMoveRequest, db: Database = Depends(get_db)):
     await db.move_squad_member(squad_member_id, request.new_squad_id)
-    return
